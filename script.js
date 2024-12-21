cacc7bae037e1f031cf0f7dbfa0d41bfc47b8539
Segun David <segdavid03@gmail.com>
2024-12-21 01:24:27 +0100

    comment redundant code

--- script.js
+++ script.js
@@ -2,72 +2,72 @@
 
 // Functions in Javascript -> Sometimes it is useful to have functions that have default values
 
-// const bookings = [];
+const bookings = [];
 
-// const createBooking = function (
-//   flightNum,
-//   numPassengers = 1,
-//   price = 199 * numPassengers
-// ) {
-//   // ES5 syntax
-//   // numPassengers = numPassengers || 1;
-//   // price = price || 199;
-//   const booking = {
-//     flightNum: flightNum,
-//     numPassengers: numPassengers,
-//     price: price,
-//   };
-//   console.log(booking);
-//   bookings.push(booking);
-// };
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5 syntax
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+  const booking = {
+    flightNum: flightNum,
+    numPassengers: numPassengers,
+    price: price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
 
-// createBooking("LH123");
-// createBooking("ph123", 3);
-// createBooking("LAG123", 4);
-// // A unique thing to notice is that in the ES6 syntax, we can use the value of our previous parameter in latter parameter
-// // Another thing to note is that we cannot skip arguments
+createBooking("LH123");
+createBooking("ph123", 3);
+createBooking("LAG123", 4);
+// A unique thing to notice is that in the ES6 syntax, we can use the value of our previous parameter in latter parameter
+// Another thing to note is that we cannot skip arguments
 
-// const flight = "LH234";
-// const david = {
-//   name: "david segun",
-//   passport: "23456789",
-// };
+const flight = "LH234";
+const david = {
+  name: "david segun",
+  passport: "23456789",
+};
 
-// const checkIn = function (flightNum, Passenger) {
-//   flightNum = "LH999";
-//   Passenger.name = "Mr. " + Passenger.name;
+const checkIn = function (flightNum, Passenger) {
+  flightNum = "LH999";
+  Passenger.name = "Mr. " + Passenger.name;
 
-//   if (Passenger.passport === "23456789") {
-//     alert("Check in!");
-//   } else {
-//     alert("wrong passport!");
-//   }
-// };
+  if (Passenger.passport === "23456789") {
+    alert("Check in!");
+  } else {
+    alert("wrong passport!");
+  }
+};
 
-// checkIn(flight, david);
-// console.log(flight);
-// console.log(david);
+checkIn(flight, david);
+console.log(flight);
+console.log(david);
 
 // FIRST CLASS FUNCTIONS AND HIGH ORDER FUNCTION -> javascript treats functions as first class citizens, this means that functions are simply values [functions are another "type" of object]
 
 // since functions are values, they are a couple interesting thing we could do with functions
 // 1 -> pass a function into another function call as a value
 // 2 -> return a function from functions
-// const btn = document.querySelector(".btn");
-// const greet = () => {
-//   alert("Hey David");
-// };
+const btn = document.querySelector(".btn");
+const greet = () => {
+  alert("Hey David");
+};
 
-// btn.addEventListener("click", greet);
+btn.addEventListener("click", greet);
 
-// // Higher Order Functions -> These are functions that either receives other functions as an argument or they return other functions
+// Higher Order Functions -> These are functions that either receives other functions as an argument or they return other functions
 
-// function count() {
-//   let counter = 0;
-//   return function () {
-//     counter++;
-//   };
-// }
+function count() {
+  let counter = 0;
+  return function () {
+    counter++;
+  };
+}
 
 const oneWord = function (str) {
   return str.replace(/ /g, "").toLowerCase();
@@ -108,11 +108,11 @@ document.body.addEventListener("click", hello);
 
 // challenge : rewrite the function usign the arrow funcrtions
 
-const greet = (greeting) => {
-  return (name) => {
-    console.log(`${greeting} ${name}`);
-  };
-};
+// const greet = (greeting) => {
+//   return (name) => {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
 
 // call the functions
 const greetHey = greet("Hello");
