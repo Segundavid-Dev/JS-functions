3fe209259533b56987bfcc45fbb12f1368a0b65b
Segun David <segdavid03@gmail.com>
2024-12-21 01:13:42 +0100

    challenge 2 completed

--- script.js
+++ script.js
@@ -187,3 +187,43 @@ const booker = secureBooking();
 console.log(booker());
 console.log(booker());
 console.log(booker());
+
+// more examples under closures in javascript
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// re assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`we ae now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
+
+// closure has priority over the scope chain
